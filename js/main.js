c7ec9488372e3802a65af4ec59875339a7b2d044
jely2002 <jw.glebbeek@gmail.com>
2020-03-04 23:37:35 +0100

    Add forceDownload class to force the browser to download images

--- js/main.js
+++ js/main.js
@@ -13,6 +13,28 @@ $(document).ready(function(){
   });
 });
 
+//Force the browser to download an image.
+function forceDownload(link){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", link.getAttribute("data-href"), true);
+  xhr.responseType = "blob";
+  xhr.onload = function(){
+    if (window.navigator && window.navigator.msSaveBlob) {
+      // Internet Explorer workaround
+      window.navigator.msSaveBlob(this.response, "Stars-over-the-frangipani.jpg");
+      return;
+    }
+    var urlCreator = window.URL || window.webkitURL;
+    var tag = document.createElement('a');
+    tag.href = urlCreator.createObjectURL(this.response);
+    tag.download = link.getAttribute("download");
+    document.body.appendChild(tag);
+    tag.click();
+    document.body.removeChild(tag);
+  };
+  xhr.send();
+}
+
 //Add animations to objects on the fly
 function animateCSS(element, animationName, callback) {
   const node = document.querySelector(element);
